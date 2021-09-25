e1701fb40d20ae39d59068160839661af330e29a
yingzhuo <yingzhuo1994@gmail.com>
2021-09-24 22:42:37 -0500

    Draw the axe and move it

--- axe_game.cpp
+++ axe_game.cpp
@@ -2,13 +2,19 @@
 int main()
 {
     // window dimensions
-    int width = 350;
-    int height = 200;
-    InitWindow(width, height, "Stephen's Window");
+    int width = 800;
+    int height = 450;
+    InitWindow(width, height, "Axe Game");
 
     // circle coordinates
-    int circle_x{175};
-    int circly_y{100};
+    int circle_x{200};
+    int circly_y{200};
+
+    // axe coordinates
+    int axe_x{400};
+    int axe_y{0};
+
+    int direction{10};
 
     SetTargetFPS(60);
     while (WindowShouldClose() == false)
@@ -19,15 +25,22 @@ int main()
         // Game logic begins
 
         DrawCircle(circle_x, circly_y, 25, BLUE);
+        DrawRectangle(axe_x, axe_y, 50, 50, RED);
 
-        if (IsKeyDown(KEY_D))
+        // move the axe
+        axe_y += direction;
+        if (axe_y > height || axe_y < 0)
         {
-            circle_x = circle_x + 10;
+            direction = -direction;
+        }
+        if (IsKeyDown(KEY_D) && circle_x < width)
+        {
+            circle_x += 10;
         }
 
-        if (IsKeyDown(KEY_A))
+        if (IsKeyDown(KEY_A) && circle_x > 0)
         {
-            circle_x = circle_x - 10;
+            circle_x -= 10;
         }
         // Game logic ends
         EndDrawing();
