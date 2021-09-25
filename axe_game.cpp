18021a95fc811b43dd63721ef6fc1ae4b81e4639
yingzhuo <yingzhuo1994@gmail.com>
2021-09-24 22:29:46 -0500

    Moving the circle

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,5 +1,35 @@
 #include "raylib.h"
 int main()
 {
-    InitWindow(350, 200, "Stephen's Window");
+    // window dimensions
+    int width = 350;
+    int height = 200;
+    InitWindow(width, height, "Stephen's Window");
+
+    // circle coordinates
+    int circle_x{175};
+    int circly_y{100};
+
+    SetTargetFPS(60);
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // Game logic begins
+
+        DrawCircle(circle_x, circly_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D))
+        {
+            circle_x = circle_x + 10;
+        }
+
+        if (IsKeyDown(KEY_A))
+        {
+            circle_x = circle_x - 10;
+        }
+        // Game logic ends
+        EndDrawing();
+    }
 }
